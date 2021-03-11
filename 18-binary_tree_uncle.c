de0558f692935b9916f2b58dd258da7af7a9888a
ANDRESOTELO <andresotelo2020@gmail.com>
2021-03-11 23:46:28 +0000

    Update uncle function validation

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -13,7 +13,7 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 
 	if (node == NULL)
 		return (NULL);
-	if (node->parent->parent->right || node->parent->parent->left)
+	if (node->parent->parent)
 	{
 		if (node->parent->parent && node->parent->parent->right != node->parent)
 			uncle = node->parent->parent->right;
